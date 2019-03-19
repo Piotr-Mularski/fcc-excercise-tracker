a620ae7b41ffe8306c54203031c7879a1b2cdad4
Piotr Mularski <piomul6473@gmail.com>
2019-03-19 19:46:38 +0100

    added async syntax to the addExercise controller

--- controlers/exerciseController.js
+++ controlers/exerciseController.js
@@ -4,28 +4,25 @@ const ExerciseModel = require('./../models/ExerciseModel');
 const UserModel = require('../models/UserModel');
 
 // TODO: refactor this block to Promises/async-await 
-exports.addExercise = (req, res) => {
-	const user = UserModel.findById(req.body.userID, (err) => {
-		if (err) {
-			res.json({ error: 'cannot find user with this ID' });
+exports.addExercise = async (req, res) => {
+	try {
+		const user = await UserModel.findById(req.body.userID);
+		if(!user) {
+			res.send('no such user');
 		} else {
-				const exerciseData = {
-					...req.body,
-					date: req.body.date ? moment(req.body.date, 'YYYY-MM-DD').toDate() : moment()
-				}
-				const exercise = new ExerciseModel(exerciseData);
-
-				exercise.save((err, data) => {
-					if (err) {
-						console.error(err);
-						res.json({ error: 'unabled to create exercise data' });
-					} else {
-						res.status(200).redirect('/')
-					}
-				});
-			};
-		});
-	};
+			const exerciseData = {
+				...req.body,
+				date: req.body.date ? moment(req.body.date, 'YYYY-MM-DD').toDate() : moment()
+			}
+			const exercise = new ExerciseModel(exerciseData);
+			await exercise.save();
+			res.status(201).redirect('/');
+		}
+	} catch(err) {
+		console.error(err);
+		res.json({ error: 'cannot save the data' });
+	}
+};
 	
 exports.getExercises = async (req, res) => {
 	try {
