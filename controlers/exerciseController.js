b103bb9c1213a4d5532fedc1757b558fc6368990
Piotr Mularski <piomul6473@gmail.com>
2019-03-19 20:04:50 +0100

    some minor fixes

--- controlers/exerciseController.js
+++ controlers/exerciseController.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 const ExerciseModel = require('./../models/ExerciseModel');
 const UserModel = require('../models/UserModel');
 
-// TODO: refactor this block to Promises/async-await 
+// TODO: refactor this block to Promises/async-await
 exports.addExercise = async (req, res) => {
 	try {
 		const user = await UserModel.findById(req.body.userID);
@@ -13,27 +13,34 @@ exports.addExercise = async (req, res) => {
 			const exerciseData = {
 				...req.body,
 				date: req.body.date ? moment(req.body.date, 'YYYY-MM-DD').toDate() : moment()
-			}
+			};
 			const exercise = new ExerciseModel(exerciseData);
-			await exercise.save();
-			res.status(201).redirect('/');
+			const savedExercise = await exercise.save();
+			res.status(201).json(savedExercise);
 		}
 	} catch(err) {
 		console.error(err);
 		res.json({ error: 'cannot save the data' });
 	}
 };
-	
+
 exports.getExercises = async (req, res) => {
 	try {
 		const user = await UserModel.findById(req.query.userid);
-		const exercises = await ExerciseModel.find({ userID: req.query.userid});
-		res.json( {
+		const exercises = await ExerciseModel.find({ userID: req.query.userid })
+		const formatedExercises = exercises.map((exercise) => {
+			return {
+				description: exercise.description,
+				duration: exercise.duration,
+				date: moment(exercise.date).format('Do MMMM YYYY')
+			};
+		});
+		res.json({
 			user: user.username,
-			exercises: exercises.map( exercise => ({ description: exercise.description, duration: exercise.duration, date: moment(exercise.date).format('Do MMMM YYYY') }))
+			exercises: formatedExercises
 		});
 	} catch(err) {
 		console.error(err);
 		res.json({ error: 'cannot fetch the data' });
 	}
-}
+};
